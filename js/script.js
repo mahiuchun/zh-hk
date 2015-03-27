91229b2cdae71670dcd62f6bc2ae9b1ddde388bd
Hill Ma <mahiuchun@users.noreply.github.com>
2015-03-26 22:58:22 -0700

    finish romanization conversion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var query_string;
+
 function query_chinese_to_cangjie3(q) {
   if (q in chinese_to_cangjie3) {
     return chinese_to_cangjie3[q].join(',');
@@ -40,15 +42,13 @@ function query_chinese_to_traditional(q) {
   return 'NA';
 }
 
-$('#search').submit(function(event){
-  var val = $('input').val();
-  val = $.trim(val);
+function render_result() {
+  var val = query_string;
   if (!val) {
     $('#result').html('');
-    event.preventDefault();
     return;
   }
-  var canto_scheme = localStorage.getItem("canto-scheme");
+  var canto_scheme = localStorage.getItem('canto-scheme');
   var content = '<table><thead>';
   content += '<tr><th>字</th><th>簡</th><th>繁</th>';
   content += '<th>倉頡</th>';
@@ -62,15 +62,42 @@ $('#search').submit(function(event){
     var cj5 = query_chinese_to_cangjie5(q);
     var cj = cangjie_merge(cj5, cj3);
     content += '<td>'+cangjie_convert(cj)+'</td>';
-    content += '<td>'+query_chinese_to_jyutping(q)+'</td>';
+    var jplst = query_chinese_to_jyutping(q).split(',')
+    if (jplst[0] !== 'NA') {
+      switch (canto_scheme) {
+        case 'yale':
+          for (var j = 0; j < jplst.length; j++) {
+            jplst[j] = cantonese_jyutping_to_yale(jplst[j]);
+          }
+          break;
+        case 'hked':
+          for (var j = 0; j < jplst.length; j++) {
+            jplst[j] = cantonese_jyutping_to_hked(jplst[j]);
+          }
+          break;
+      }
+    }
+    var canto = jplst.join(',');
+    content += '<td>'+canto+'</td>';
     content += '<td>'+query_chinese_to_mandarin(q)+'</td>';
     content += '</tr>';
   }
   content += '</tbody></table>';
   $('#result').html(content);
+}
+
+$('#search').submit(function(event){
+  var val = $('input').val();
+  query_string = $.trim(val);
+  render_result();
   event.preventDefault();
 });
 
+if (localStorage.getItem('canto-scheme') === null) {
+  localStorage.setItem('canto-scheme', 'jyutping');
+}
+$('#canto-scheme').val(localStorage.getItem('canto-scheme'));
 $('#canto-scheme').change(function(event){
-  localStorage.setItem("canto-scheme", this.value);
+  localStorage.setItem('canto-scheme', this.value);
+  render_result();
 })
